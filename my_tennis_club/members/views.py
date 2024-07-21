b7b7f4f1623569d4d0d59b9fe6ae04ff5da43095
Hiteshj1693 <Hiteshjethava161611@gmail.com>
2024-07-21 21:42:07 +0530

    update

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, loader
 from django.http import HttpResponse
 # Create your views here.
 
 def members(request):
-    return HttpResponse("Hello world!")
+    # return HttpResponse("Hello world!")
+    template = loader.get_template('myfirst.html')
+    return HttpResponse(template.render())
