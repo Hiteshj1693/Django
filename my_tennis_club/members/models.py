7ddb1acb307c7e1c299f0bb480cf9c54518821c6
Hitesh Jethava <Hiteshjethava161611@gmail.com>
2024-10-07 12:33:52 +0530

    update

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -4,3 +4,22 @@ from django.db import models
 class Member(models.Model):
   firstname = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
+  email = models.EmailField(unique=True)
+  password = models.CharField(max_length=255)
+  phone = models.CharField(max_length=20)
+  address = models.CharField(max_length=255)
+  role = models.CharField(max_length=255)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  def __str__(self):
+    return self.firstname + ' ' + self.lastname
+  class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    image = models.ImageField(upload_to='products/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+      return self.name
+    
--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -7,3 +7,39 @@ def members(request):
     # return HttpResponse("Hello world!")
     template = loader.get_template('myfirst.html')
     return HttpResponse(template.render())
+def index(request):
+    return HttpResponse("Hello world!")
+def home(request):
+    return HttpResponse("Hello world!")
+def about(request):
+    return HttpResponse("Hello world!")
+def contact(request):
+    return HttpResponse("Hello world!")
+def services(request):
+    return HttpResponse("Hello world!")
+def login(request):
+    return HttpResponse("Hello world!")
+def register(request):
+    return HttpResponse("Hello world!")
+def logout(request):
+    return HttpResponse("Hello world!")
+def profile(request):
+    return HttpResponse("Hello world!")
+def dashboard(request):
+    return HttpResponse("Hello world!")
+def add(request):
+    return HttpResponse("Hello world!")
+def view(request):
+    return HttpResponse("Hello world!")
+def delete(request):
+    return HttpResponse("Hello world!")
+def update(request):
+    return HttpResponse("Hello world!")
+def search(request):
+    return HttpResponse("Hello world!")
+def viewall(request):
+    return HttpResponse("Hello world!")
+def viewone(request):
+    return HttpResponse("Hello world!")
+def viewall(request):
+    return HttpResponse("Hello world!")
